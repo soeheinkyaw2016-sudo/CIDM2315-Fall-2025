d6c28729372fc142e06efb549adad3ea5a187f7c
soeheinkyaw2016-sudo <soeheinkyaw2016@gmail.com>
2025-08-30 01:22:54 -0500

    summit Homework1 (part1)

---
+++ Homework1(part 1).cs
@@ -0,0 +1,12 @@
+﻿namespace week1;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        double X,Y,Z;
+        X=2.5; Y = 3.3;
+        Z=4*X*X+3*Y;
+        Console.WriteLine("The value of Z is"+Z);
+    }
+}
